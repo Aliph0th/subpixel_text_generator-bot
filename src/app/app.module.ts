382bace38023317cb0a44790efb099364bed0035
Aliph0th <theblacklord05@gmail.com>
2024-11-03 00:51:38 +0300

    feat: add postgres telegraf session

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppUpdate } from './app.update';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Postgres } from '@telegraf/session/pg';
 import { TelegrafModule } from 'nestjs-telegraf';
-import { errorMiddleware } from '../common/middlewares';
 import { session } from 'telegraf';
+import { errorMiddleware } from '../common/middlewares';
 import { GeneratorModule } from '../generator/generator.module';
-import { TextModeScene } from '../scenes/text';
 import { AdminScene } from '../scenes/admin';
+import { TextModeScene } from '../scenes/text';
+import { AppUpdate } from './app.update';
 
 @Module({
    imports: [
-      ConfigModule.forRoot({ isGlobal: true }),
+      ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env', '.env.postgres'] }),
       TelegrafModule.forRootAsync({
          imports: [ConfigModule],
          inject: [ConfigService],
          useFactory(configService: ConfigService) {
+            const store = Postgres({
+               host: configService.getOrThrow('POSTGRES_HOST'),
+               port: configService.getOrThrow('POSTGRES_PORT'),
+               database: configService.getOrThrow('POSTGRES_DB'),
+               user: configService.getOrThrow('POSTGRES_USER'),
+               password: configService.getOrThrow('POSTGRES_PASSWORD')
+            });
             return {
                token: configService.getOrThrow('TOKEN'),
-               middlewares: [errorMiddleware, session()],
+               middlewares: [errorMiddleware, session({ store })],
                options: { handlerTimeout: 3.6e6 }
             };
          }
